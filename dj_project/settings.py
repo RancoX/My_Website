9fb9618f469caf763ae348d3b15cbd8402fbef61
Ranco <xxc199548@gmail.com>
2022-07-26 07:39:10 +1000

    working v1 without profile pic

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -21,10 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7s3(w-q))9(#ca0e16^#f*0k7i)1gckcy!jg$c2h*0-f#j92cj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# if os.getenv('GAE_APPLICATION', None):
+#     DEBUG = False
+# else:
+#     DEBUG = True
 DEBUG = True
+if os.getenv('GAE_APPLICATION', None):
+    DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -77,23 +83,39 @@ WSGI_APPLICATION = 'dj_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'maindb',
-        'USER':'ranco',
-        'PASSWORD':'Hf201701!',
-        'HOST':'/cloudsql/my-django-blog-356513:australia-southeast1:django-blog'
+if os.getenv('GAE_APPLICATION', None):
+    # Running on production App Engine, so connect to Google Cloud SQL using the unix socket at /cloudsql/<your-cloudsql-connection string>
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'maindb',
+            'USER':os.environ.get('USER_DB'),
+            'PASSWORD':os.environ.get('USER_PW'),
+            'HOST':'/cloudsql/my-django-blog-356513:australia-southeast1:django-blog'
+        }
     }
-}
+else:
+    # Running locally so connect to either a local Sqlite3 instance or connect 
+    # to Cloud SQL via the proxy.  To start the proxy via command line: 
+    # .\cloud_sql_proxy.exe -instances="my-django-blog-356513:australia-southeast1:django-blog"=tcp:5432
+    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': BASE_DIR / 'db.sqlite3',
+    #     }
+    # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'maindb',
+            'USER':os.environ.get('USER_DB'),
+            'PASSWORD':os.environ.get('USER_PW'),
+            'HOST':'127.0.0.1',
+            'PORT':'5432'
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -128,7 +150,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -151,5 +174,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='xxc199548@gmail.com'
-EMAIL_HOST_PASSWORD='Hf201701'
+EMAIL_HOST_USER=os.environ.get('USER_EMAIL')
+EMAIL_HOST_PASSWORD=os.environ.get('USER_PW')
