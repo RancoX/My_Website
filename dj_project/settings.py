60ab6438cfeb83285349bdfb8312933b19ea04f1
RancoX <rancoxu@gmail.com>
2024-08-05 00:37:40 +1000

    turn off debug

--- dj_project/settings.py
+++ dj_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY=config['SECRET_KEY']
 DB_INFO = config['DATABASE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = [
